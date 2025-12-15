7d55c6e83402399ca70f644e0d8f3bc853f1d144
Mujithaba <mujithabaep772@gmail.com>
2025-12-15 12:50:32 +0400

    some comments removed

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import AppRoutes from "./routes/AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
+  
   return (
-    <Router>
+    <Router> 
       <AppRoutes />
     </Router>
   );
